d581733392167dde12e0b5e41a46c2795bbaf581
MohsenSami <mohsensami88@gmail.com>
2023-08-19 22:06:20 +0330

    Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 function orderCake() {
@@ -25,3 +28,16 @@ const reducer = (state = initialState, action) => {
             return state;
     }
 };
+
+const store = createStore(reducer);
+console.log('Initial State: ', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Update state: ', store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
