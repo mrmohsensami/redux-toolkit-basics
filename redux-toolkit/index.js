241ebe8c9c0e6aa16379299b0c30aac4c03174f9
MohsenSami <mohsensami88@gmail.com>
2023-08-23 20:55:29 +0330

    redux Logger

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -4,7 +4,7 @@ const icecreamActions = require('./features/icecream/icecreamSlice').icecreamAct
 
 console.log('Initial State: ', store.getState());
 const unsubscribe = store.subscribe(() => {
-    console.log('Updated State: ', store.getState());
+    // console.log('Updated State: ', store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
